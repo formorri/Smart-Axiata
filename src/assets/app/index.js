717980325b6e2dc4e57f2b8a51fdfd3f48929e10
User <user@Jerrys-iMac-2.local>
2022-07-15 18:41:32 +0800

    completed about and CEO page

--- src/assets/app/index.js
+++ src/assets/app/index.js
@@ -1,6 +1,6 @@
-gsap.registerPlugin(ScrollTrigger);
-const scrub = 3;
-const startTrigger = "top 10%";
+// gsap.registerPlugin(ScrollTrigger);
+// const scrub = 3;
+// const startTrigger = "top 10%";
 let tl1=gsap.timeline()
 tl1.from("#reveal", {delay:.5, duration:2, width: 0, ease: "easeInOut"})
 .from(".artwork", {yPercent:100, duration:.8, autoAlpha:0})
@@ -13,3 +13,20 @@ tl1.from("#reveal", {delay:.5, duration:2, width: 0, ease: "easeInOut"})
 //     let yAxis = (window.innerHeight / 2 - e.pageY) / 20;
 //     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 // });
+
+/*****CountUp Number****/
+$(".count").countUp({
+	delay: 10,
+	/* The delay in milliseconds per number count up */
+	time: 2300,
+	/*The total duration of the count up animation */
+});
+
+$(".slider-counter a").click(function() {
+    jQuery(document).ready(function($) {
+        $('.count').countUp({
+            time: 1000
+        })
+    })
+
+})	
