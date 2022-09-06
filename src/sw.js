631bbb97e225004f1c7dbdabc5337c7d1bca7a9d
User <user@Jerrys-iMac-2.local>
2022-09-06 18:18:52 +0800

    redo because accidentally commited to branch

--- src/sw.js
+++ src/sw.js
@@ -1,122 +1,91 @@
-const staticCacheName = "site-static-v1";
-const dynamicCacheName = "site-dynamic-v1";
+const staticCacheName = "site-static-v2";
+const dynamicCacheName = "site-dynamic-v2";
+// const directoryName= "/development/PWA-test";
+const directoryName = "";
 const assets = [
-    "/development/PWA-test/",
-    "/development/PWA-test/index.html",
-    "/development/PWA-test/fallback.html",
-    // "/about-us.html",
-    // "/beyond-short-term-profits.html",
-    // "/CEO-message.html",
-    // "/humanitarian-support.html",
-    // "/nurturing-people.html",
-    // "/planet-and-society.html",
-    // "/process-excellence.html",
+    (directoryName + "/"),
+    (directoryName + "/index.html"),
+    (directoryName + "/fallback.html"),
 
     // ALL CSS
-    // "/css/colors.css",
-    // "/css/components.css",
-    "/development/PWA-test/css/fonts.css",
-    "/development/PWA-test/css/main.css",
+    (directoryName + "/css/fonts.css"),
+    (directoryName + "/css/main.css"),
 
     // ALL APP
-    "/development/PWA-test/assets/app/countup.min.js",
-    "/development/PWA-test/assets/app/index.js",
-    "/development/PWA-test/assets/app/jquery-3.6.0.min.js",
-    "/development/PWA-test/assets/app/uikit-icons.min.js",
-    "/development/PWA-test/assets/app/uikit.min.js",
-    "/development/PWA-test/assets/app/waypoints.min.js",
+    (directoryName + "/assets/app/countup.min.js"),
+    (directoryName + "/assets/app/index.js"),
+    (directoryName + "/assets/app/jquery-3.6.0.min.js"),
+    (directoryName + "/assets/app/uikit-icons.min.js"),
+    (directoryName + "/assets/app/uikit.min.js"),
+    (directoryName + "/assets/app/waypoints.min.js"),
 
     // ALL FONTS
-    "/development/PWA-test/assets/fonts/icomoon.eot",
-    "/development/PWA-test/assets/fonts/icomoon.svg",
-    "/development/PWA-test/assets/fonts/icomoon.ttf",
-    "/development/PWA-test/assets/fonts/icomoon.woff",
-    "/development/PWA-test/assets/fonts/selection.json",
-    "/development/PWA-test/assets/fonts/Ubuntu-Bold.eot",
-    "/development/PWA-test/assets/fonts/Ubuntu-Bold.ttf",
-    "/development/PWA-test/assets/fonts/Ubuntu-Bold.woff",
-    "/development/PWA-test/assets/fonts/Ubuntu-Bold.woff2",
-    "/development/PWA-test/assets/fonts/Ubuntu-BoldItalic.eot",
-    "/development/PWA-test/assets/fonts/Ubuntu-BoldItalic.ttf",
-    "/development/PWA-test/assets/fonts/Ubuntu-BoldItalic.woff",
-    "/development/PWA-test/assets/fonts/Ubuntu-BoldItalic.woff2",
-    "/development/PWA-test/assets/fonts/Ubuntu-Italic.eot",
-    "/development/PWA-test/assets/fonts/Ubuntu-Italic.ttf",
-    "/development/PWA-test/assets/fonts/Ubuntu-Italic.woff",
-    "/development/PWA-test/assets/fonts/Ubuntu-Italic.woff2",
-    "/development/PWA-test/assets/fonts/Ubuntu-Light.eot",
-    "/development/PWA-test/assets/fonts/Ubuntu-Light.ttf",
-    "/development/PWA-test/assets/fonts/Ubuntu-Light.woff",
-    "/development/PWA-test/assets/fonts/Ubuntu-Light.woff2",
-    "/development/PWA-test/assets/fonts/Ubuntu-LightItalic.eot",
-    "/development/PWA-test/assets/fonts/Ubuntu-LightItalic.ttf",
-    "/development/PWA-test/assets/fonts/Ubuntu-LightItalic.woff",
-    "/development/PWA-test/assets/fonts/Ubuntu-LightItalic.woff2",
-    "/development/PWA-test/assets/fonts/Ubuntu-Medium.eot",
-    "/development/PWA-test/assets/fonts/Ubuntu-Medium.ttf",
-    "/development/PWA-test/assets/fonts/Ubuntu-Medium.woff",
-    "/development/PWA-test/assets/fonts/Ubuntu-Medium.woff2",
-    "/development/PWA-test/assets/fonts/Ubuntu-MediumItalic.eot",
-    "/development/PWA-test/assets/fonts/Ubuntu-MediumItalic.ttf",
-    "/development/PWA-test/assets/fonts/Ubuntu-MediumItalic.woff",
-    "/development/PWA-test/assets/fonts/Ubuntu-MediumItalic.woff2",
-    "/development/PWA-test/assets/fonts/Ubuntu-Regular.eot",
-    "/development/PWA-test/assets/fonts/Ubuntu-Regular.ttf",
-    "/development/PWA-test/assets/fonts/Ubuntu-Regular.woff",
-    "/development/PWA-test/assets/fonts/Ubuntu-Regular.woff2",
+    (directoryName + "/assets/fonts/icomoon.eot"),
+    (directoryName + "/assets/fonts/icomoon.svg"),
+    (directoryName + "/assets/fonts/icomoon.ttf"),
+    (directoryName + "/assets/fonts/icomoon.woff"),
+    (directoryName + "/assets/fonts/selection.json"),
+    (directoryName + "/assets/fonts/Ubuntu-Bold.eot"),
+    (directoryName + "/assets/fonts/Ubuntu-Bold.ttf"),
+    (directoryName + "/assets/fonts/Ubuntu-Bold.woff"),
+    (directoryName + "/assets/fonts/Ubuntu-Bold.woff2"),
+    (directoryName + "/assets/fonts/Ubuntu-BoldItalic.eot"),
+    (directoryName + "/assets/fonts/Ubuntu-BoldItalic.ttf"),
+    (directoryName + "/assets/fonts/Ubuntu-BoldItalic.woff"),
+    (directoryName + "/assets/fonts/Ubuntu-BoldItalic.woff2"),
+    (directoryName + "/assets/fonts/Ubuntu-Italic.eot"),
+    (directoryName + "/assets/fonts/Ubuntu-Italic.ttf"),
+    (directoryName + "/assets/fonts/Ubuntu-Italic.woff"),
+    (directoryName + "/assets/fonts/Ubuntu-Italic.woff2"),
+    (directoryName + "/assets/fonts/Ubuntu-Light.eot"),
+    (directoryName + "/assets/fonts/Ubuntu-Light.ttf"),
+    (directoryName + "/assets/fonts/Ubuntu-Light.woff"),
+    (directoryName + "/assets/fonts/Ubuntu-Light.woff2"),
+    (directoryName + "/assets/fonts/Ubuntu-LightItalic.eot"),
+    (directoryName + "/assets/fonts/Ubuntu-LightItalic.ttf"),
+    (directoryName + "/assets/fonts/Ubuntu-LightItalic.woff"),
+    (directoryName + "/assets/fonts/Ubuntu-LightItalic.woff2"),
+    (directoryName + "/assets/fonts/Ubuntu-Medium.eot"),
+    (directoryName + "/assets/fonts/Ubuntu-Medium.ttf"),
+    (directoryName + "/assets/fonts/Ubuntu-Medium.woff"),
+    (directoryName + "/assets/fonts/Ubuntu-Medium.woff2"),
+    (directoryName + "/assets/fonts/Ubuntu-MediumItalic.eot"),
+    (directoryName + "/assets/fonts/Ubuntu-MediumItalic.ttf"),
+    (directoryName + "/assets/fonts/Ubuntu-MediumItalic.woff"),
+    (directoryName + "/assets/fonts/Ubuntu-MediumItalic.woff2"),
+    (directoryName + "/assets/fonts/Ubuntu-Regular.eot"),
+    (directoryName + "/assets/fonts/Ubuntu-Regular.ttf"),
+    (directoryName + "/assets/fonts/Ubuntu-Regular.woff"),
+    (directoryName + "/assets/fonts/Ubuntu-Regular.woff2"),
 
     // ALL ICONS
-    "/development/PWA-test/assets/icons/6-download-global.svg",
-    "/development/PWA-test/assets/icons/7-donate-global.svg",
-    // "/assets/icons/1-about-search.svg",
-    // "/assets/icons/2-about1.svg",
-    // "/assets/icons/3-about2.svg",
-    // "/assets/icons/4-about3.svg",
-    // "/assets/icons/5-about4.svg",
+    (directoryName + "/assets/icons/6-download-global.svg"),
+    (directoryName + "/assets/icons/7-donate-global.svg"),
 
     // ALL IMAGES
-    "/development/PWA-test/assets/images/logo-1.svg",
-    "/development/PWA-test/assets/images/logo-2.svg",
-    "/development/PWA-test/assets/images/play.svg",
-    "/development/PWA-test/assets/images/navbar-background.svg",
-    "/development/PWA-test/assets/images/navbar-download.svg",
-    "/development/PWA-test/assets/images/navbar-home.svg",
-    "/development/PWA-test/assets/images/navbar-share.svg",
-    "/development/PWA-test/assets/images/frontpage-artwork.png",
-    "/development/PWA-test/assets/images/frontpage-artwork.svg",
-    "/development/PWA-test/assets/images/frontpage-CEO.svg",
-    "/development/PWA-test/assets/images/frontpage-sustainability-1.svg",
-    "/development/PWA-test/assets/images/frontpage-sustainability-2.svg",
-    "/development/PWA-test/assets/images/frontpage-sustainability-3.svg",
-    "/development/PWA-test/assets/images/frontpage-sustainability-4.svg",
-    "/development/PWA-test/assets/images/frontpage-waves.svg",
-    "/development/PWA-test/assets/images/frontpage-title.svg",
-    // "/assets/images/about-background.png",
-    // "/assets/images/about-CEO-background.svg",
-    // "/assets/images/about-CEO.svg",
-    // "/assets/images/beyond-header.svg",
-    // "/assets/images/beyond-icon.svg",
-    // "/assets/images/humanitarian-thumbnail.png",
-    // "/assets/images/nurturing-header.svg",
-    // "/assets/images/nurturing-icon.svg",
-    // "/assets/images/planet-header.svg",
-    // "/assets/images/planet-icon.svg",
-    // "/assets/images/process-header.svg",
-    // "/assets/images/process-icon.svg",
-    // "/assets/images/sponsor-1.png",
-    // "/assets/images/sponsor-2.png",
-    // "/assets/images/sponsor-3.png",
-    // "/assets/images/sponsor-4.png",
-    // "/assets/images/sponsor-5.png",
-    // "/assets/images/sponsor-6.png",
+    (directoryName + "/assets/images/logo-1.svg"),
+    (directoryName + "/assets/images/logo-2.svg"),
+    (directoryName + "/assets/images/play.svg"),
+    (directoryName + "/assets/images/navbar-background.svg"),
+    (directoryName + "/assets/images/navbar-download.svg"),
+    (directoryName + "/assets/images/navbar-home.svg"),
+    (directoryName + "/assets/images/navbar-share.svg"),
+    (directoryName + "/assets/images/frontpage-artwork.png"),
+    (directoryName + "/assets/images/frontpage-CEO.jpg"),
+    (directoryName + "/assets/images/frontpage-sustainability-1.jpg"),
+    (directoryName + "/assets/images/frontpage-sustainability-2.jpg"),
+    (directoryName + "/assets/images/frontpage-sustainability-3.jpg"),
+    (directoryName + "/assets/images/frontpage-sustainability-4.jpg"),
+    (directoryName + "/assets/images/frontpage-waves.svg"),
+    (directoryName + "/assets/images/frontpage-title.svg"),
 
     // UIKIT
-    "/development/PWA-test/assets/UIKIT/uikit.min.css",
+    (directoryName + "/assets/UIKIT/uikit.min.css"),
 
     // ALL VIDEO
     // "/assets/video/ceo.mp4",
     // "/assets/video/testimonial.mp4",
-    "/development/PWA-test/assets/video/year-in-review.mp4",
+    (directoryName + "/assets/video/year-in-review.mp4"),
 
     // ALL EXTERNAL LINKS
     "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/gsap.min.js",
@@ -126,13 +95,13 @@ const assets = [
 
 // cache size limit function. Loops to the first key and deletes it untill it meets cache limit
 const limitCacheSize = (name, size) => {
-  caches.open(name).then(cache => {
-    cache.keys().then(keys => {
-      if(keys.length > size){
-        cache.delete(keys[0]).then(limitCacheSize(name, size));
-      }
+    caches.open(name).then(cache => {
+        cache.keys().then(keys => {
+            if (keys.length > size) {
+                cache.delete(keys[0]).then(limitCacheSize(name, size));
+            }
+        });
     });
-  });
 };
 
 
@@ -142,8 +111,8 @@ self.addEventListener("install", evt => {
     console.log("Service event has been installed");
     evt.waitUntil(
         caches.open(staticCacheName).then(cache => {
-            console.log("Caching shell assets");
             cache.addAll(assets);
+            console.log("Caching shell assets");
         })
     );
 
@@ -182,8 +151,8 @@ self.addEventListener("fetch", evt => {
                 })
             });
         }).catch(() => {
-            if(evt.request.url.indexOf(".html") > -1){
-                return caches.match("/development/PWA-test/fallback.html");
+            if (evt.request.url.indexOf(".html") > -1) {
+                return caches.match(directoryName + "/fallback.html");
             }
         })
     )
