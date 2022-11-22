477c8b05c6fc94aee53042634aee532abbddccc1
formorri <kampfriegule@gmail.com>
2022-11-22 17:35:57 +0800

    Added one carousel to sustainability page

--- src/assets/app/app.js
+++
@@ -1,7 +0,0 @@
-const directoryName="/development/PWA-test";
-// const directoryName= "";
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register(directoryName + "/sw.js")
-        .then((reg) => console.log("Service worker registered",reg))
-        .catch((err)=> console.log("service worker not registered",err));
-}
--- src/assets/app/index.js
+++ src/assets/app/index.js
@@ -1,35 +1,43 @@
-// gsap.registerPlugin(ScrollTrigger);
-// const scrub = 3;
-// const startTrigger = "top 10%";
-let tl1=gsap.timeline()
-tl1.from("#reveal", {delay:.5, duration:2, width: 0, ease: "easeInOut"})
-.from(".artwork", {yPercent:100, duration:.8, autoAlpha:0})
-.from(".video-button", {yPercent:200, duration:.2, autoAlpha:0}, "<")
-.from(".title", {yPercent:100, delay:"-.4", duration: 0.6,autoAlpha:0});
+// disappearing navbar when viewport < 960px
+$(window).on('resize',() => {
+    var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    var header = $(".navbar-top");
+    var lastScroll = 0;
+    $(window).scroll(() => {
+        var currentScroll = $(this).scrollTop()
+        if (vw < 960) {
+            if (currentScroll > lastScroll && currentScroll > 100) {
+                header.addClass("scrolled");
+            } else {
+                header.removeClass("scrolled");
+            }
+            lastScroll = currentScroll;
+        } 
+    });
+}).resize();
 
-// let card = document.querySelector('.artwork');
-//   document.addEventListener('mousemove', function (e) {
-//     let xAxis = (window.innerWidth / 2 - e.pageX) / 40;
-//     let yAxis = (window.innerHeight / 2 - e.pageY) / 20;
-//     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-// });
+let tl1 = gsap.timeline()
+tl1.from("#reveal", { delay: .5, duration: 2, width: 0, ease: "easeInOut" })
+    .from(".artwork", { yPercent: 100, duration: .8, autoAlpha: 0 })
+    .from(".video-button", { yPercent: 200, duration: .2, autoAlpha: 0 }, "<")
+    .from(".title", { yPercent: 100, delay: "-.4", duration: 0.6, autoAlpha: 0 });
 
 
 /*****CountUp Number****/
 $(".count").countUp({
-	delay: 10,
-	/* The delay in milliseconds per number count up */
-	time: 2300,
-	/*The total duration of the count up animation */
+    delay: 10,
+    /* The delay in milliseconds per number count up */
+    time: 1500,
+    /*The total duration of the count up animation */
 
 });
 
 
-$(".slider-counter a").click(function() {
-    jQuery(document).ready(function($) {
+$(".slider-counter a").click(function () {
+    jQuery(document).ready(function ($) {
         $('.count').countUp({
             time: 1000
         })
     })
 
-})	
+})
