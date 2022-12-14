996eb4c151b7b170454fe26d44121f1266f49b87
User <user@Users-iMac.local>
2022-12-14 18:34:20 +0800

    checkpoint before adding new message from leader

--- src/assets/app/index.js
+++ src/assets/app/index.js
@@ -1,66 +1,64 @@
-$(window).on('resize',function(){location.reload();});
+
+// force reload when resize
+$(window).on('resize', function () { location.reload(); });
 
 // disappearing navbar when viewport < 960px
 // $(window).on('resize', () => {
-    var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-    var header = $(".navbar-top");
-    var lastScroll = 0;
-    $(window).scroll(() => {
-        var currentScroll = $(this).scrollTop()
-        if (vw < 960) {
-            if (currentScroll > lastScroll && currentScroll > 100) {
-                header.addClass("scrolled");
-            } else {
-                header.removeClass("scrolled");
-            }
-            lastScroll = currentScroll;
+var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+var header = $(".navbar-top");
+var lastScroll = 0;
+$(window).scroll(() => {
+    var currentScroll = $(this).scrollTop()
+    if (vw < 960) {
+        if (currentScroll > lastScroll && currentScroll > 100) {
+            header.addClass("scrolled");
+        } else {
+            header.removeClass("scrolled");
         }
-    });
+        lastScroll = currentScroll;
+    }
+});
 // }).resize();
 
 // animate gif on scroll
-// Beyond chart gif
-$(window).scroll(() => {
-    var current = $(this).scrollTop(),
-        path1 = 'assets/images/beyond.gif',
-        visible = $('.scroll-gif1').css('opacity') != 0;
-
+function gifReload(gif, path, visible) {
+    var current = $(this).scrollTop();
     if (current > 0) {
-        if (!visible) $('.scroll-gif1').attr('src', path1).fadeTo(400, 1);
+        if (!visible) gif.attr('src', path).fadeTo(400, 1);
     }
-    else if (visible) $('.scroll-gif1').fadeTo(0, 0);
-});
+    else if (visible) gif.fadeTo(0, 0);
+}
 
-// Nurturing chart gif
-$(window).scroll(() => {
-    var current = $(this).scrollTop(),
-        path2 = 'assets/images/nurturing.gif',
-        visible = $('.scroll-gif2').css('opacity') != 0;
-
-    if (current > 0) {
-        if (!visible) $('.scroll-gif2').attr('src', path2).fadeTo(400, 1);
+$(window).on('scroll resize load', () => {
+    // Beyond chart gif
+    {
+        var gif = $('.scroll-gif1'),
+            path = 'assets/images/beyond.gif',
+            visible = gif.css('opacity') != 0;
+        gifReload(gif, path, visible);
+    };
+    // Nurturing chart gif
+    {
+        var gif = $('.scroll-gif2'),
+            path = 'assets/images/nurturing.gif',
+            visible = gif.css('opacity') != 0;
+        gifReload(gif, path, visible);
+    };
+    // Planet chart gif
+    {
+        var gif = $('.scroll-gif3'),
+            path = 'assets/images/planet.gif',
+            visible = gif.css('opacity') != 0;
+        gifReload(gif, path, visible);
     }
-    else if (visible) $('.scroll-gif2').fadeTo(0, 0);
-});
-
-// Planet chart gif
-$(window).scroll(() => {
-    var current = $(this).scrollTop(),
-        path3 = 'assets/images/planet.gif',
-        visible = $('.scroll-gif3').css('opacity') != 0;
-
-    if (current > 0) {
-        if (!visible) $('.scroll-gif3').attr('src', path3).fadeTo(400, 1);
-    }
-    else if (visible) $('.scroll-gif3').fadeTo(0, 0);
 });
 
 
 let tl1 = gsap.timeline()
 // tl1.from("#reveal", { delay: .5, duration: 2, width: 0, ease: "easeInOut" })
-    // tl1.from(".artwork", { delay: .5, yPercent: 100, duration: .8, autoAlpha: 0, ease: "easeInOut"  })
-    tl1.from(".video-button", { delay: .5, yPercent: 200, duration: .2, autoAlpha: 0 })
-    // .from(".title", { yPercent: 100, delay: "-.4", duration: 0.6, autoAlpha: 0 });
+// tl1.from(".artwork", { delay: .5, yPercent: 100, duration: .8, autoAlpha: 0, ease: "easeInOut"  })
+tl1.from(".video-button", { delay: .5, yPercent: 200, duration: .2, autoAlpha: 0 })
+// .from(".title", { yPercent: 100, delay: "-.4", duration: 0.6, autoAlpha: 0 });
 
 
 /*****CountUp Number****/
@@ -81,3 +79,4 @@ $(".slider-counter a").click(function () {
     })
 
 })
+
